708d41f902bcb730de150a5885a00c0a633c2d23
James Friend <james@jsdf.co>
2016-10-13 08:05:35 -0700

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var hnGraphQLMiddleware = require('./hnGraphQLMiddleware');
+var cors = require('cors');
 
 var app = express();
+
+app.use(cors());
+
 app.use('/graphql', hnGraphQLMiddleware);
 
 module.exports = (customPort) => {
